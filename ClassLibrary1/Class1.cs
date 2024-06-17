a307331a152d5259b7e3756fcf126940f0c2c64d
William Bonazzoli <w.bonazzoli@gmail.com>
2024-06-17 23:02:30 +0200

    Improved Perfprmance

--- ClassLibrary1/Class1.cs
+++ ClassLibrary1/Class1.cs
@@ -1,10 +1,9 @@
 ﻿using ClassLibrarySecond;
-using ClassLibrarySecond.Interfaces;
 using AttributeGenerator;
 
 namespace ClassLibrary1;
 
-[AutoImplementProperties(nameof(IUserInterface), nameof(IUserInterface2))]
+[AutoImplementProperties(typeof(IUserInterface), typeof(IUserInterface2))]
 public partial class UserClass
 {
     public string UserProp { get; set; }
